16b43c2bce578a33d1a007f50d47593f962f1371
Steve Tricanowicz <steven.tricanowicz@gmail.com>
2010-10-30 18:29:45 -0400

    Began implementing post viewing; as yet incomplete

---
+++ includes/get_post.php
@@ -0,0 +1,46 @@
+<?
+	/*
+	 * void
+	 * get_post($id)
+	 *
+	 * Gets post with message_id $id and outputs the html for it
+	 */
+	function get_post($id, $mysqli)
+	{
+		// prepare SQL
+		$sql = "SELECT * FROM users WHERE message_id=?";
+
+		$stmt = mysqli_stmt_init($mysqli);
+		$stmt->prepare($sql);
+		$stmt->bind_param("i", $id);
+
+		// execute query
+		$stmt->execute();
+		$stmt->bind_result($uid);
+		
+		// if we found a row, remember user and redirect to portfolio
+		while ($stmt->fetch())
+		{
+			echo "						<tr>";
+			echo "							<td class=\"posthorzpadding\"></td>";
+			echo "							<td  class=\"postdisplay\">";
+			echo "								<font size=\"1\">Posted October 23, 2010 at 4:27pm by a junior male.</font>";
+			echo "								<br/><br/>";
+			echo "								Never have I ever... slept with Brian Plancher. <br/><br/>";
+			echo "								<form>";
+			echo "									<input class=\"responsebutton\" type=\"submit\" value=\"Me neither...\" />&nbsp;&nbsp;&nbsp;";
+			echo "									<input class=\"responsebutton\" type=\"submit\" value=\"I have!\" />";
+			echo "							  	</form>";
+			echo "							<td/>";
+			echo "							<td class=\"posthorzpadding\"></td>";
+			echo "						</tr>";
+
+			exit;
+		}
+
+		// else report error
+		apologize("There was an error. Please try again in a few minutes.");
+
+	}
+
+?>
